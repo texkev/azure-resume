b2d95aa80076d0c49a67e33bb18c3ccc363393dd
texkev <kevin.chechak@gmail.com>
2023-06-29 12:53:50 -0500

    Create azure function API settings

--- frontend/main.js
+++ frontend/main.js
@@ -2,11 +2,12 @@ window.addEventListener('DOMContentLoaded', (event) =>{
     getVisitCount();
 })
 
-const functionApi = 'http://localhost:7071/api/GetResumeCounter';
+const functionApiURL = 'https://rescounter.azurewebsites.net/api/GetResumeCounter?code=9wm24LnudEOC2Kd77oO0jglmelNOyUVNTn-igq1kGSeCAzFugtsUfA=='
+const localfunctionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionApi).then(response => {
+    fetch(functionApiURL).then(response => {
         return response.json()
     }).then(response =>{
         console.log("Website called function API.");
